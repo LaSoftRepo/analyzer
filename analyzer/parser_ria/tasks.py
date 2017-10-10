c892cb16b4cc9f5a12b76ef0fe6e6722e4f422fb
Andrii Varunok <andrii.varunok@lasoft.org>
2017-10-10 04:31:14 +0300

    fix1

--- analyzer/parser_ria/tasks.py
+++ analyzer/parser_ria/tasks.py
@@ -3,7 +3,7 @@ from celery import shared_task
 from parser_ria.parser import ParserRia
 
 
-@shared_task(name='parser-AUTO.RIA')
-def start_parser_olx():
+@shared_task(name='parser-AUTO-RIA')
+def start_parser_ria():
     parser = ParserRia()
     parser.start()
