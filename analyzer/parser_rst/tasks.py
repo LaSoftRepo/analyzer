f13b446d05fa52dd2c69aa91a8b00071ba23ee78
Andrii Varunok <andrii.varunok@lasoft.org>
2017-11-10 04:15:15 +0200

    fix celery task

--- analyzer/parser_rst/tasks.py
+++ analyzer/parser_rst/tasks.py
@@ -4,8 +4,8 @@ from parser_rst.parser import ParserRst
 from settings_analyzer.models import StatusSiteParse
 
 
-@shared_task(name='parser-AUTO-RIA')
-def start_parser_ria():
+@shared_task(name='parser-RST-UA')
+def start_parser_rst():
     site = StatusSiteParse.objects.filter(name__exact='rst.ua')
     if site.exists():
         site = site.first()
